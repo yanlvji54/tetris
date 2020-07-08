3461317fa230faffe4640b5bc986e6c9b1511aba
yanlvji54 <631797108@163.com>
2020-07-08 23:12:05 +0800

    feat: add transform function

--- src/core/block.ts
+++ src/core/block.ts
@@ -2,23 +2,21 @@ import config from '../config/config'
 // block 本质是一个坐标控制器， 依赖一个中心原点， 根据规则计算周围点的坐标， 通过坐标的位置更新视图。
 export class Block {
   type:Array<Array<number>> = [
-    [0,0,0,1,0,2,0,3],// 长条形
-    [0,0,1,0,1,1,2,1],// 左z
-    [0,1,1,1,1,0,2,0],// 右z
-    [0,0,0,1,1,0,1,1],// 正方形
-    [0,0,0,1,1,1,2,1],// L型
-    [2,0,0,1,1,1,2,1],// J型
-    [1,0,0,1,1,1,2,1],// 土字型
+    [0, -1, 0, 0, 0, 1, 0, 2],// 长条形
+    [-1, -1, 0, -1, 0, 0, 1, 0],// 左z
+    [0, 0, -1, 0, 0, -1, 1, -1],// 右z
+    [0, 0, 0, 1, 1, 0, 1, 1],// 正方形
+    [-1, -1, 0, -1, 1, -1, -1, 0],// L型
+    [-1, -1, 0, -1, 1, -1, 1, 0],// J型
+    [0, 0, 0, -1, -1, 0, 1, 0],// 土字型
   ]
   originX:number = 4;
-  originY:number = 0;
+  originY:number = 1;
   position:Array<number> = [];
   shape: number = -1;
   // 创建方块
   createBlock () {
-    // test
-    // this.shape = 6;
-    this.shape = Math.floor(Math.random() * 4);
+    this.shape = Math.floor(Math.random() * 7);
     this.getBlockPosition();
     this.drop();
   }
@@ -27,6 +25,9 @@ export class Block {
     let position = this.type[this.shape].map((item, index) => {
       return index % 2 !== 0 ? this.originY + item : this.originX + item;
     })
+    // 边界判定
+    const boundaryX = position.map((item, index) => index % 2 === 0 ? item : NaN).filter(item => item === item);
+    if (Math.max(...boundaryX) > 9 || Math.min(...boundaryX) < 0) return;
     
     this.position = position.map((item, index) => {
       if (index % 2 === 0) {
@@ -65,40 +66,10 @@ export class Block {
 
   transform () {
     let pos = [];
-    switch (this.shape) {
-      case 4:
-        break;
-      case 0:
-        // (y, x)
-        if (this.originX >= 7) return;
-        pos = [];
-        this.type[this.shape].forEach((item, index) => {
-          index % 2 === 0 ? pos[index + 1] = item : pos[index - 1] = item
-        })
-        this.type[this.shape] = pos;
-        this.getBlockPosition();
-        break;
-      // case 
-      case 1:
-        // (y, -x)
-        if (this.originX === 9) break;
-        pos = [];
-        this.type[this.shape].forEach((item, index) => {
-          index % 2 === 0 ? pos[index + 1] = item : pos[index - 1] = item * -1
-        })
-        this.type[this.shape] = pos;
-        this.getBlockPosition();
-        break;
-      case 2:
-        // (2 - y, x)
-        if (this.originX === 0) return;
-        pos = [];
-        this.type[this.shape].forEach((item, index) => {
-          index % 2 === 0 ? pos[index + 1] = item : pos[index - 1] = 2 - item 
-        })
-        this.type[this.shape] = pos;
-        this.getBlockPosition();
-        break;
-    }
-  }
+    this.type[this.shape].forEach((item, index) => {
+      index % 2 === 0 ? pos[index + 1] = item : pos[index - 1] = item * -1
+    })
+    this.type[this.shape] = pos;
+    this.getBlockPosition();
+  };
 }
